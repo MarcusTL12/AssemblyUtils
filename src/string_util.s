a2fb7d5161f7661a1f77f86f4cc1257b3702210c
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-10-22 18:57:14 +0200

    Changes

--- src/string_util.s
+++ src/string_util.s
@@ -1,7 +1,24 @@
+.globl strlen
 .globl str_eq
 .globl parse_str_int_n
 .globl parse_str_int
+.globl str_split_char
 .text
+
+# Parameters:
+# %rdi: Pointer to null terminated string
+# Returns length of string
+strlen:
+    xor %rax, %rax
+    xor %r10, %r10
+    dec %rax
+    strlen_loop:
+        inc %rax
+        mov (%rdi), %r10b
+        inc %rdi
+        cmp $0, %r10b
+        jne strlen_loop
+    ret
 
 # Parameters:
 # %rdi: Pointer to null terminated string 1
@@ -84,3 +101,106 @@ parse_str_int:
     call parse_str_int_n
     ret
 
+# Parameters:
+# %rdi: pointer to string
+# %rsi: length of string
+# %rdx: character to split on
+# %rcx: pointer to buffer to put the substring pointers into
+# %r8: pointer to buffer to put the substring lenghts into
+# %r9: length of buffers
+# Returns:
+# Integer number of substrings found
+str_split_char:
+    push %r12
+    # %r10 stores the length of the current substring
+    xor %r10, %r10
+    # %r11 stores the start of the current substring
+    mov %rdi, %r11
+    # %rax stores the current number of substrings
+    xor %rax, %rax
+    
+    # The main loop of the function.
+    # Each iteration of this loop should result in a single substring
+    # (if any exist)
+    str_split_char_loop:
+        # Each substring starts with a non-split character
+        # This loop looks for one such
+        str_split_char_find_nonsplit:
+            # Check if the current character is a split-char
+            # If not exit the loop
+            # or increment the string pointer
+            xor %r12, %r12
+            movb (%rdi), %r12b
+            cmp %rdx, %r12
+            jne str_split_char_find_nonsplit_end
+            # Increment the string pointer and decrement the remaining
+            # length of the string
+            inc %rdi
+            dec %rsi
+            
+            # Check if the remaining length of the string is 0
+            # If so, exit the outer loop
+            test %rsi, %rsi
+            jz str_split_char_loop_end
+            
+            jmp str_split_char_find_nonsplit
+        str_split_char_find_nonsplit_end:
+        
+        # If we've come here, we've found a non-split character that is the
+        # start of a substring, so this is to be added to the buffer
+        mov %rdi, (%rcx)
+        add $8, %rcx
+        
+        # And we increment the number of strings found
+        inc %rax
+        
+        # Set length of current string to 0
+        xor %r10, %r10
+        
+        # This loop looks for the end of the current substring
+        str_split_char_find_substring:
+            # Check if current char is splitting
+            # If it is, jump out of the loop
+            # If not, increment string pointer
+            xor %r12, %r12
+            movb (%rdi), %r12b
+            cmp %rdx, %r12
+            je str_split_char_find_substring_end
+            # Increment the string pointer and decrement the remaining
+            # length of the string
+            inc %rdi
+            dec %rsi
+            
+            # Increment length of current substring
+            inc %r10
+            
+            # Check if we've reached the end of the string
+            # If so, jump out of loop, but add the final substring
+            # to the buffers
+            test %rsi, %rsi
+            jz str_split_char_loop_end_and_add_final
+            jmp str_split_char_find_substring
+        str_split_char_find_substring_end:
+        
+        # If we've come here we have found a complete substring
+        # and should add its length into the buffer
+        mov %r10, (%r8)
+        add $8, %r8
+        
+        # Remaining buffers reduced by one
+        dec %r9
+        
+        # Check if the buffers are full (None remaining)
+        # If so, jump out of outer loop
+        test %r9, %r9
+        jz str_split_char_loop_end
+        
+        # If we've reached the end of the loop without any breaks
+        # jump to the top of the loop
+        jmp str_split_char_loop
+    str_split_char_loop_end_and_add_final:
+    # If we've here, the final string is to be added to the buffer
+    mov %r10, (%r8)
+    str_split_char_loop_end:
+    pop %r12
+    ret
