a0361ac1416fcffef0dce8daf0e4a8eae291f842
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-10-27 22:50:15 +0100

    min

--- src/math_util.s
+++ src/math_util.s
@@ -1,4 +1,5 @@
-
+.globl min_n
+.globl min_3
 .text
 
 # Parameters:
@@ -10,5 +11,27 @@ min_n:
     add $8, %rdi
     dec %rsi
     min_n_loop:
+        test %rsi, %rsi
+        jz min_n_loop_end
+        mov (%rdi), %r8
+        cmp %rax, %r8
+        cmovl %r8, %rax
         
+        add $8, %rdi
+        dec %rsi
+        jmp min_n_loop
+    min_n_loop_end:
+    ret
+
+# Parameters:
+# %rdi: First number
+# %rsi: Second number
+# %rdx: Third number
+# Returns minimum of these three
+min_3:
+    mov %rdi, %rax
+    cmp %rax, %rsi
+    cmovl %rsi, %rax
+    cmp %rax, %rdx
+    cmovl %rdx, %rax
     ret
