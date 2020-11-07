2de679206f0c0910903e7237805485c7ecab37b4
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-11-07 12:30:56 +0100

    _

--- src/hashmap_util.s
+++ src/hashmap_util.s
@@ -1,11 +1,13 @@
 .globl hashmap_test
+.globl simple_hash
+.globl simple_compare
 .text
 
 # Structure of hashmap item:
 # Size: 24
 # Int(8 bytes): Length of key
 # Pointer (8 bytes): Key
-# (Pointer) (8 bytes): Data
+# (8 bytes): Data (For example Int or Pointer)
 
 
 # Parameters:
@@ -27,7 +29,14 @@ simple_hashmap_new:
 # Parameters:
 # %rdi: Pointer to struct
 # %rsi: seed0
+# %rdx: seed1
 simple_hash:
+    mov %rdx, %rcx
+    mov %rsi, %rdx
+    mov %rdi, %r8
+    mov 8(%r8), %rdi
+    mov 0(%r8), %rsi
+    call hashmap_sip
     ret
 
 # Parameters:
@@ -57,9 +66,80 @@ simple_compare:
 
 
 hashmap_test:
+    push %r12
     xor %rdi, %rdi
     call simple_hashmap_new
+    mov %rax, %r12
     
-    mov %rax, %rdi
+    .set mapstruct, 24
+    .set point, mapstruct + 16
+    push %rbp
+    mov %rsp, %rbp
+    sub $point, %rsp
+    
+    # Setting values
+    
+    # map[(1, 1)] = 3
+    movq $16, -mapstruct(%rbp)
+    lea -point(%rbp), %r8
+    movq $1, -point(%rbp)
+    movq $1, -point + 8(%rbp)
+    mov %r8, -mapstruct + 8(%rbp)
+    movq $3, -mapstruct + 16(%rbp)
+    
+    mov
+    
+    # mov %r12, %rdi
+    # lea -mapstruct(%rbp), %rsi
+    # call hashmap_set
+    
+    # # map[(1, 2)] = 1
+    # movq $1, -point(%rbp)
+    # movq $2, -point + 8(%rbp)
+    
+    # movq $1, -mapstruct + 16(%rbp)
+    
+    # mov %r12, %rdi
+    # lea -mapstruct(%rbp), %rsi
+    # call hashmap_set
+    
+    # # map[(2, 1)] = 4
+    # movq $2, -point(%rbp)
+    # movq $1, -point + 8(%rbp)
+    
+    # movq $1, -mapstruct + 16(%rbp)
+    
+    # mov %r12, %rdi
+    # lea -mapstruct(%rbp), %rsi
+    # call hashmap_set
+    
+    # # map[(2, 2)] = 1
+    # movq $2, -point(%rbp)
+    # movq $2, -point + 8(%rbp)
+    
+    # movq $1, -mapstruct + 16(%rbp)
+    
+    # mov %r12, %rdi
+    # lea -mapstruct(%rbp), %rsi
+    # call hashmap_set
+    
+    # # Reading values
+    
+    # # point = (1, 2)
+    # movq $1, -point(%rbp)
+    # movq $2, -point + 8(%rbp)
+    
+    # # print(map[point])
+    # mov %r12, %rdi
+    # lea -mapstruct(%rbp), %rsi
+    # call hashmap_get
+    # mov %rax, %rdi
+    # call print_int_dec_s
+    # call newline
+    
+    leave
+    
+    mov %r12, %rdi
     call hashmap_free
+    pop %r12
     ret
